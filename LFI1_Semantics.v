ed5f9ed40a09d993912b3ff86c6451729a18ca6e
helena <helenavargastannuri@gmail.com>
2024-07-12 21:22:47 -0300

    updated Syntax

--- LFI1_Semantics.v
+++ LFI1_Semantics.v
@@ -1,48 +1,46 @@
 From Coq Require Export String.
 
 Inductive TruthValue : Set :=
-    | One
-    | Half
-    | Zero.
-
+  | One
+  | Half
+  | Zero.
 
 Definition andlf (a b : TruthValue) : TruthValue :=
-    match a, b with
-    | Zero, _  => Zero
-    | _, Zero  => Zero
-    | One, One => One
-    | _, _     => Half
-    end.
+  match a, b with
+  | Zero, _  => Zero
+  | _, Zero  => Zero
+  | One, One => One
+  | _, _     => Half
+  end.
 
 Definition orlf (a b : TruthValue) : TruthValue :=
-    match a, b with
-    | One, _     => One
-    | _, One     => One
-    | Zero, Zero => Zero
-    | _, _       => Half
-    end.
+  match a, b with
+  | One, _     => One
+  | _, One     => One
+  | Zero, Zero => Zero
+  | _, _       => Half
+  end.
 
 Definition implf (a b : TruthValue) : TruthValue :=
-    match a, b with
-    | Zero, _ => One
-    | _, One  => One
-    | _, Half => Half
-    | _, Zero => Zero
-    end.
+  match a, b with
+  | Zero, _ => One
+  | _, One  => One
+  | _, Half => Half
+  | _, Zero => Zero
+  end.
 
 Definition neglf (a : TruthValue) : TruthValue :=
-    match a with
-    | One  => Zero
-    | Half => Half
-    | Zero => One
-    end.
-
+  match a with
+  | One  => Zero
+  | Half => Half
+  | Zero => One
+  end.
 
 Definition inconslf (a : TruthValue) : TruthValue :=
-    match a with
-    | Half => One
-    | _    => Zero
-    end.
+  match a with
+  | Half => One
+  | _    => Zero
+  end.
 
 Definition bimplf (a b : TruthValue) : TruthValue := 
 andlf (implf a b) (implf b a). 
--- LFI1_Syntax.v
+++ LFI1_Syntax.v
@@ -1,13 +1,15 @@
 From Coq Require Export String.
 Require Import Arith List ListSet.
 
+
 Inductive Formula : Set :=
-    | Lit    : nat -> Formula
-    | Neg    : Formula -> Formula
-    | And    : Formula -> Formula -> Formula
-    | Or     : Formula -> Formula -> Formula
-    | Imp    : Formula -> Formula -> Formula
-    | Incons : Formula -> Formula.
+  | Lit    : nat -> Formula
+  | Neg    : Formula -> Formula
+  | And    : Formula -> Formula -> Formula
+  | Or     : Formula -> Formula -> Formula
+  | Imp    : Formula -> Formula -> Formula
+  | Incons : Formula -> Formula.
+
 
 Notation " x → y " := 
 (Imp x y) (at level 80, right associativity).
@@ -24,31 +26,70 @@ Notation " x ∨ y " :=
 Notation " ¬ x " := 
 (Neg x) (at level 9, right associativity, format "¬ x").
 
-Notation " • x " := 
-(Incons x) (at level 9, right associativity, format "• x").
+Notation " ∙ x " := 
+(Incons x) (at level 9, right associativity, format "∙ x").
 
 Notation " # x " :=
-(Lit x) (at level 1, no associativity, x constr at level 1, format "# x").
+(Lit x) (at level 2, no associativity, x constr at level 1, format "# x").
 
 
 Fixpoint size (f : Formula) : nat :=
-    match f with
-    | Lit x    => 1
-    | Neg a    => 1 + size a
-    | And a b  => 1 + size a + size b
-    | Or a b   => 1 + size a + size b
-    | Imp a b  => 1 + size a + size b
-    | Incons a => 1 + size a
-    end.
+ match f with
+ | Lit x    => 1
+ | Neg a    => 1 + size a
+ | And a b  => 1 + size a + size b
+ | Or a b   => 1 + size a + size b
+ | Imp a b  => 1 + size a + size b
+ | Incons a => 1 + size a
+ end.
 
 Fixpoint literals (f : Formula) : set nat :=
-    match f with
-    | Lit x    => set_add eq_nat_dec x (empty_set nat)
-    | Neg a    => literals a
-    | And a b  => set_union eq_nat_dec (literals a) (literals b)
-    | Or a b   => set_union eq_nat_dec (literals a) (literals b)
-    | Imp a b  => set_union eq_nat_dec (literals a) (literals b)
-    | Incons a => literals a
-    end.
+  match f with
+  | Lit x    => set_add eq_nat_dec x (empty_set nat)
+  | Neg a    => literals a
+  | And a b  => set_union eq_nat_dec (literals a) (literals b)
+  | Or a b   => set_union eq_nat_dec (literals a) (literals b)
+  | Imp a b  => set_union eq_nat_dec (literals a) (literals b)
+  | Incons a => literals a
+  end.
+
+Fixpoint evenP (n : nat) : Prop :=
+  match n with
+  | S (S n) => evenP n
+  | O => True
+  | S O => False
+  end.
+
+Fixpoint formulaSAT (v : nat -> Prop) (f : Formula) : Prop :=
+  match f with
+  | #x    => v x
+  | a ∧ b => (formulaSAT v a) /\ (formulaSAT v b)
+  | a ∨ b => (formulaSAT v a) \/ (formulaSAT v b)
+  | a → b => (formulaSAT v a) -> (formulaSAT v b)
+  | ∙a    => formulaSAT v a /\ ~ (formulaSAT v a)
+  | ¬a    => ~(formulaSAT v a)
+  end.
+
+Definition theory := set Formula.
+
+
+Fixpoint theorySAT (v : nat -> Prop) (Γ : theory) : Prop :=
+  match Γ with
+  | nil => True
+  | h :: t => (formulaSAT v h) /\ (theorySAT v t)
+  end.
+
+Definition entails (Γ : theory) (f : Formula) : Prop :=
+forall (v : nat -> Prop), theorySAT v Γ -> formulaSAT v f.
+
+Notation " A |= B " :=
+(entails A B) (at level 110, no associativity).
+
+Example teste : forall (Γ : theory) (A B : Formula), 
+  ((∙A) :: Γ) |= A ∧ ¬A.
+Proof.
+  intros.
+  unfold entails. intros. apply H. 
+Qed. 
 
 
