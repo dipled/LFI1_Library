d0e91f7388c144e2070fc64fbe08350e29ffc2c4
helena <helenavargastannuri@gmail.com>
2024-07-13 10:13:09 -0300

    cheguei a conclusão de que NAO SEI

--- LFI1_Semantics.v
+++ LFI1_Semantics.v
@@ -1,4 +1,6 @@
 From Coq Require Export String.
+Require Import Arith List ListSet.
+Require Import LFI1_Syntax.
 
 Inductive TruthValue : Set :=
   | One
@@ -45,3 +47,46 @@ Definition inconslf (a : TruthValue) : TruthValue :=
 Definition bimplf (a b : TruthValue) : TruthValue := 
 andlf (implf a b) (implf b a). 
 
+
+
+Fixpoint evenP (n : nat) : Prop :=
+  match n with
+  | S (S n) => evenP n
+  | O => True
+  | S O => False
+  end.
+
+Fixpoint formulaSAT (v : nat -> Prop) (f : Formula) : Prop :=
+  match f with
+  | #x    => v x
+  | a ∧ b => (formulaSAT v a) /\ (formulaSAT v b)
+  | a ∨ b => (formulaSAT v a) \/ (formulaSAT v b)
+  | a → b => ~(formulaSAT v a) \/ (formulaSAT v b)
+  | ¬a    => ~(formulaSAT v a) \/ (formulaSAT v ∙a)
+  | ∙a    => (formulaSAT v a) /\ (formulaSAT v ¬a)
+  | .~a   => (formulaSAT v a)
+  end.
+
+Definition theory := set Formula.
+
+
+Fixpoint theorySAT (v : nat -> Prop) (Γ : theory) : Prop :=
+  match Γ with
+  | nil => True
+  | h :: t => (formulaSAT v h) /\ (theorySAT v t)
+  end.
+
+Definition entails (Γ : theory) (f : Formula) : Prop :=
+forall (v : nat -> Prop), theorySAT v Γ -> formulaSAT v f.
+
+Notation " A |= B " :=
+(entails A B) (at level 110, no associativity).
+
+Example teste : forall (Γ : theory) (A B : Formula), 
+  ((∙A) :: Γ) |= A ∧ ¬A.
+Proof.
+  intros.
+  unfold entails. intros. apply H. 
+Qed. 
+
+Compute (inconslf Half).
