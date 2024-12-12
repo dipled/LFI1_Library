6d2d409909553f2ca06917d6190f7fcea20dd71f
helena <helenavargastannuri@gmail.com>
2024-12-12 19:04:58 -0300

    deletando arquivos inuteis

--- Ensemble/.CoqMakeFile.d
+++
@@ -1,9 +0,0 @@
-Semantics.vo Semantics.glob Semantics.v.beautified Semantics.required_vo: Semantics.v Language.vo
-Semantics.vio: Semantics.v Language.vio
-Semantics.vos Semantics.vok Semantics.required_vos: Semantics.v Language.vos
-Syntax.vo Syntax.glob Syntax.v.beautified Syntax.required_vo: Syntax.v Language.vo
-Syntax.vio: Syntax.v Language.vio
-Syntax.vos Syntax.vok Syntax.required_vos: Syntax.v Language.vos
-Language.vo Language.glob Language.v.beautified Language.required_vo: Language.v 
-Language.vio: Language.v 
-Language.vos Language.vok Language.required_vos: Language.v 
