baeb30192a58e37a90348a6ddf178f3a3331c29e
helena <helenavargastannuri@gmail.com>
2024-12-17 12:05:08 -0300

    oay

--- coq/Countability.v
+++ coq/Countability.v
@@ -1,6 +1,9 @@
 Require Import Epsilon.
 (** Defining countability for inductive types, inspired by
+  https://github.com/QinxiangCao/Countable_PaperSubmission
+  and
   https://github.com/guodk/A-Comprehensive-Formalization-of-Propositional-Logic-in-Coq
+
  *)
 
 Definition function_injective {A B : Type} (f: A -> B): Prop :=
@@ -8,9 +11,6 @@ Definition function_injective {A B : Type} (f: A -> B): Prop :=
 
 Definition function_surjective {A B : Type} (f: A -> B): Prop :=
   forall b, exists a, f a = b.
-
-Inductive function_bijective {A B : Type} (f: A -> B): Prop :=
-  | fun_bij : function_injective f -> function_surjective f -> function_bijective f.
 
 Definition inverse_function {A B : Type} (f : A -> B) (g : B -> A) :=
   forall x : A, g (f x) = x.
@@ -32,21 +32,18 @@ Record bijection (A B: Type): Type := Build_bijection {
 }.
 
 Definition injection_trans {A B C : Type} (f1: injection A B) (f2: injection B C): injection A C.
-  apply (Build_injection A C (fun (x : A) => f2 (f1 x))). unfold function_injective. intros.
-  pose proof (in_inj A B f1). unfold function_injective in H0.
-  pose proof (in_inj B C f2). unfold function_injective in H1.
-  apply H1 in H. apply H0 in H. apply H.
+  apply (Build_injection A C (fun (x : A) => f2 (f1 x))). unfold function_injective. 
+  intros. apply (in_inj B C f2) in H. apply (in_inj A B f1) in H. apply H.
 Defined.
 
 Definition bijection_injection {A B : Type} (f : bijection A B) : injection A B.
-  apply (Build_injection A B f). unfold function_injective. intros.
-  pose proof (in_bij A B f). unfold function_injective in H0. apply H0.
-  apply H.
+  apply (Build_injection A B f). apply (in_bij A B f).
 Defined.
 
+Print bijection_injection.
+
 Definition bijection_surjection {A B : Type} (f : bijection A B) : surjection A B.
-  apply (Build_surjection A B f). unfold function_surjective. intros.
-  pose proof (su_bij A B f). unfold function_surjective in H. apply H.
+  apply (Build_surjection A B f). apply (su_bij _ _ f).
 Defined.
 
 Definition bijection_sym {A B : Type} (f : bijection A B): bijection B A.
