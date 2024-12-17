d87640e8b57fc0466f2f1932a7d36c352f01ee7f
helena <helenavargastannuri@gmail.com>
2024-12-17 11:06:01 -0300

    aprendendo a mexer com records

--- coq/Completeness.v
+++ coq/Completeness.v
@@ -426,13 +426,13 @@ Qed.
 
 Fixpoint Gamma_i 
   (Γ : Ensemble Formula) (i : nat) (f: nat -> Formula) (φ : Formula) : Ensemble Formula :=
-match i with
-| O   => Γ
-| S n => match (strong_lem (((Gamma_i Γ n f φ) ∪ [f n]) ⊢ φ)) with
-         | left _  => (Gamma_i Γ n f φ)
-         | right _ => (Gamma_i Γ n f φ) ∪ [f n]
-         end
-end.
+  match i with
+  | O   => Γ
+  | S n => match (strong_lem (((Gamma_i Γ n f φ) ∪ [f n]) ⊢ φ)) with
+           | left _  => (Gamma_i Γ n f φ)
+           | right _ => (Gamma_i Γ n f φ) ∪ [f n]
+           end
+  end.
 
 Definition Delta
   (Γ : Ensemble Formula) (f: nat -> Formula) (φ : Formula) : Ensemble Formula :=
@@ -446,7 +446,7 @@ fun (x : Formula) => exists n : nat, x ∈ (Gamma_i Γ n f φ).
 *)
 
 (** Defining countability for inductive types, inspired by
-    https://github.com/QinxiangCao/Countable_PaperSubmission
+  https://github.com/guodk/A-Comprehensive-Formalization-of-Propositional-Logic-in-Coq
  *)
 
 Definition function_injective {A B : Type} (f: A -> B): Prop :=
@@ -455,25 +455,64 @@ Definition function_injective {A B : Type} (f: A -> B): Prop :=
 Definition function_surjective {A B : Type} (f: A -> B): Prop :=
   forall b, exists a, f a = b.
 
+Inductive function_bijective {A B : Type} (f: A -> B): Prop :=
+  | fun_bij : function_injective f -> function_surjective f -> function_bijective f.
+
+Definition inverse_function {A B : Type} (f : A -> B) (g : B -> A) :=
+  forall x : A, g (f x) = x.
+
 Record injection (A B: Type): Type := Build_injection {
-  inj_f:> A -> B;
-  in_inj: function_injective inj_f
+  inj_f :> A -> B;
+  in_inj : function_injective inj_f
 }.
 
 Record surjection (A B: Type): Type := Build_surjection {
-  sur_f:> A -> B;
-  su_surj: function_surjective sur_f
+  sur_f :> A -> B;
+  su_surj : function_surjective sur_f
 }.
 
 Record bijection (A B: Type): Type := Build_bijection {
-  bij_f:> A -> B;
-  in_bij: function_injective bij_f;
-  su_bij: function_surjective bij_f
+  bij_f :> A -> B;
+  in_bij : function_injective bij_f;
+  su_bij : function_surjective bij_f;
 }.
 
 Definition injection_trans {A B C : Type} (f1: injection A B) (f2: injection B C): injection A C.
-  apply (Build_injection A C (fun a => f2 (f1 a))).
-  unfold function_injective. intros. apply (in_inj B C f2) in H. 
-  apply (in_inj A B f1) in H. apply H.
+  apply (Build_injection A C (fun (x : A) => f2 (f1 x))). unfold function_injective. intros.
+  pose proof (in_inj A B f1). unfold function_injective in H0.
+  pose proof (in_inj B C f2). unfold function_injective in H1.
+  apply H1 in H. apply H0 in H. apply H.
 Defined.
 
+Definition bijection_injection {A B : Type} (f : bijection A B) : injection A B.
+  apply (Build_injection A B f). unfold function_injective. intros.
+  pose proof (in_bij A B f). unfold function_injective in H0. apply H0.
+  apply H.
+Defined.
+
+Definition bijection_surjection {A B : Type} (f : bijection A B) : surjection A B.
+  apply (Build_surjection A B f). unfold function_surjective. intros.
+  pose proof (su_bij A B f). unfold function_surjective in H. apply H.
+Defined.
+
+Definition bijection_sym {A B : Type} (f : bijection A B): bijection B A.
+  pose proof (su_bij A B f); unfold function_surjective in H.
+  apply (Build_bijection _ _ 
+  (
+    fun (x : B) => 
+        proj1_sig (constructive_indefinite_description _ ((su_bij A B f) x)))
+  ).
+    - unfold function_injective. intros.
+      destruct constructive_indefinite_description in H0.
+      destruct constructive_indefinite_description in H0.
+      simpl in H0. rewrite <- e, <- e0, H0. reflexivity.
+    - unfold function_surjective. intros.
+      exists (f b). destruct constructive_indefinite_description.
+      simpl. apply (in_bij _ _ f) in e. apply e.
+Qed.
+      
+
+
+
+
+Defined.
