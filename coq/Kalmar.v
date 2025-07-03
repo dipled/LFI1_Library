4717e7035ed00936b781bc56078bb5ea9fa869e8
helena <helenavargastannuri@gmail.com>
2025-07-03 10:36:23 -0300

    ok

--- coq/Kalmar.v
+++ coq/Kalmar.v
@@ -127,7 +127,9 @@ Theorem kalmar: Δᵥ ⊢ kalmar_function v φ.
 Proof.
   unfold Δᵥ. unfold valuation in v_is_valuation. induction φ.
   - simpl. apply Premisse. apply Im_intro with (#a); reflexivity.
-  - destruct_conjunction v_is_valuation. simpl. unfold preserveNeg in L2. specialize (L2 f). unfold kalmar_function at 2. unfold kalmar_function at 2 in IHf. destruct (v ¬f); destruct (v f); try discriminate L2.
+  - destruct_conjunction v_is_valuation. simpl. unfold preserveNeg in L2. 
+  specialize (L2 f). unfold kalmar_function at 2. unfold kalmar_function at 2 in IHf. 
+  destruct (v ¬f); destruct (v f); try discriminate L2.
     + assert (Im (atoms f) (kalmar_function v) ⊢ ∘f). apply (MP _ ∘f ∧ ¬f).
       apply (AxiomInstance _ (Ax4 _ _)). assumption.
       assert (Im (atoms f) (kalmar_function v) ⊢ ¬f). apply (MP _ ∘f ∧ ¬f).
@@ -144,7 +146,50 @@ Proof.
       apply (AxiomInstance _ (ce _)). assumption. apply (MP _ ¬¬f).
       apply (MP _ ∘¬f). apply (AxiomInstance _ (Ax3 _ _)).
       apply (MP _ ∘f). apply T1. apply H. apply H1.
-  -
-      
+  - destruct_conjunction v_is_valuation. simpl. unfold preserveAnd in L1.
+    specialize (L1 f1 f2).
+    assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ kalmar_function v f1).
+      apply (lfi1_monotonicity _ (Im (atoms f1) (kalmar_function v))).
+      split. assumption. unfold Included. intros. destruct H. apply Im_intro with (x := x). left. assumption. assumption. 
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ kalmar_function v f2).
+      apply (lfi1_monotonicity _ (Im (atoms f2) (kalmar_function v))).
+      split. assumption. unfold Included. intros. destruct H0. 
+      apply Im_intro with (x := x). right. assumption. assumption.
+    unfold kalmar_function at 2.
+    unfold kalmar_function at 2 in H. unfold kalmar_function at 2 in H0.
+    clear IHf1. clear IHf2.
+    destruct (v f1 ∧ f2), (v f1), (v f2); try discriminate L1.
+    + assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ∘f1 ∧ f1 ∧ (∘f2 ∧ f2)).
+      apply (MP _ ∘f2 ∧ f2). apply (MP _ ∘f1 ∧ f1).
+      apply (AxiomInstance _ (Ax3 _ _)). assumption. assumption.
+      apply (MP _ ∘f1 ∧ f1 ∧ (∘f2 ∧ f2)). apply T4. assumption.
+    + assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ f2 ∧ ¬f2).
+      apply (MP _ ¬∘f2). apply (AxiomInstance _ (ci _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ f2).
+      apply (MP _ f2 ∧ ¬f2). apply (AxiomInstance _ (Ax4 _ _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ¬f2).
+      apply (MP _ f2 ∧ ¬f2). apply (AxiomInstance _ (Ax5 _ _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ∘f1).
+      apply (MP _ ∘f1 ∧ f1). apply (AxiomInstance _ (Ax4 _ _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ f1).
+      apply (MP _ ∘f1 ∧ f1). apply (AxiomInstance _ (Ax5 _ _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ (f1 ∧ f2)).
+      apply (MP _ f2). apply (MP _ f1). apply (AxiomInstance _ (Ax3 _ _)).
+      assumption. assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ¬(f1 ∧ f2)).
+      apply (MP _ ¬f1 ∨ ¬f2). apply (AxiomInstance _ (negland2 _ _)).
+      apply (MP _ ¬f2). apply (AxiomInstance _ (Ax7 _ _)). assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ∘(f1 ∧ f2) → ¬∘(f1 ∧ f2)).
+      apply deduction_metatheorem. apply (MP _ ¬(f1 ∧ f2)). apply (MP _ (f1 ∧ f2)).
+      apply (MP _ ∘(f1 ∧ f2)). apply (AxiomInstance _ (bc1 _ _)).
+      apply Premisse. right. reflexivity. 
+      apply (lfi1_monotonicity _( Im (atoms f1 ∪ atoms f2) (kalmar_function v))). split. assumption. left. assumption.
+      apply (lfi1_monotonicity _( Im (atoms f1 ∪ atoms f2) (kalmar_function v))). split. assumption. left. assumption.
+      assert (Im (atoms f1 ∪ atoms f2) (kalmar_function v) ⊢ ¬∘(f1 ∧ f2) → ¬∘(f1 ∧ f2)).
+      apply id. apply (MP _ (∘(f1 ∧ f2) ∨ ¬∘(f1 ∧ f2))).
+      apply (MP _ ¬∘(f1 ∧ f2) → ¬∘(f1 ∧ f2)). apply (MP _ ∘(f1 ∧ f2) → ¬∘(f1 ∧ f2)).
+      apply (AxiomInstance _ (Ax8 _ _ _)). assumption. assumption.
+      apply (AxiomInstance _ (Ax10 _)).
+
 End Kalmar_like.
 
